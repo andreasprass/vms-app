2cba4bc6bc2e49ac3086804292eb76971c53124c
andreasprass <andreasprasetya29@gmail.com>
2024-09-29 07:16:36 +0700

    add options in chart

--- resources/views/livewire/chart-visit-component.blade.php
+++ resources/views/livewire/chart-visit-component.blade.php
@@ -1,4 +1,38 @@
-<div class="p-4 bg-white rounded-3xl">
+<div class="p-4 bg-white rounded-3xl" x-data="{visit_dropDown: false}">
+    <div class="flex justify-between items-center">
+        <h3 class="font-medium text-blue-950">Visit</h3>
+        <div class="relative inline-block text-left">
+            <div>
+                <button 
+                class="ps-2 pt-1 pe-2 pb-1 font-semibold text-blue-900 flex items-center bg-slate-100 rounded-full"
+                @click="visit_dropDown = !visit_dropDown">
+                    <h3 class="text-xs me-1">Options </h3> 
+                    <x-heroicon-o-chevron-down class="w-4 h-4"/>
+                </button>
+            </div>
+          
+            <!--
+              Dropdown menu, show/hide based on menu state.
+          
+              Entering: "transition ease-out duration-100"
+                From: "transform opacity-0 scale-95"
+                To: "transform opacity-100 scale-100"
+              Leaving: "transition ease-in duration-75"
+                From: "transform opacity-100 scale-100"
+                To: "transform opacity-0 scale-95"
+            -->
+            <div 
+            x-show="visit_dropDown"
+            class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-xl text-blue-900 bg-white  shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" >
+              <div class="py-1" >
+                <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" -->
+                <a href="#" class="block px-4 py-2 text-sm">Daily</a>
+                <a href="#" class="block px-4 py-2 text-sm">Monthly</a>
+                <a href="#" class="block px-4 py-2 text-sm">Yearly</a>
+              </div>
+            </div>
+          </div>
+    </div>
     <canvas id="chart1"></canvas>
 </div>
   
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,16 +1,18 @@
 <div class="w-full relative min-h-screen flex" 
     x-data="{ sideNav: $persist(false), isSmallScreen: window.innerWidth < 768 }"
     x-init="
-    // Initialize sideNav state
-    if (window.innerWidth >= 768) { 
-        sideNav = true; 
-    } else {
-        sideNav = sideNav;
-    }
-    // Watch for changes in screen size
-    $watch('isSmallScreen', value => { if (!value) sideNav = true; else sideNav = false; });
+        // Initialize sideNav state
+        if (window.innerWidth >= 768) { 
+            sideNav = true; 
+        } 
+        // Watch for changes in screen size
+        $watch('isSmallScreen', value => {
+            if (!value) {
+                sideNav = true; // Keep sideNav open on larger screens
+            }
+        });
     "
-    @resize.window="isSmallScreen = window.innerWidth < 768; sideNav = !isSmallScreen">
+    @resize.window="isSmallScreen = window.innerWidth < 768; if (!isSmallScreen) sideNav = true">
 
     {{-- Sidenav Start --}}
     <div 
@@ -21,17 +23,15 @@
         x-transition:leave="transition ease-in duration-300"
         x-transition:leave-start="translate-x-0"
         x-transition:leave-end="-translate-x-full"
+        class="absolute h-full w-4/6 p-8 flex flex-col justify-between shadow-lg bg-blue-900 text-white text-bold sm:w-3/6 md:w-3/12 md:static md:min-h-screen xl:w-1/6"
+        @click.outside="sideNav = false"
+        >
 
-        {{-- Apply click.outside only if it's a small screen --}}
-        :class="{'@click.outside': isSmallScreen}" 
-        @click.outside="isSmallScreen && (sideNav = false)"
-
-        class="z-50 absolute h-full w-4/6 p-8 flex flex-col justify-between shadow-lg bg-blue-900 text-white text-bold sm:w-3/6 md:w-3/12 md:static md:min-h-screen xl:w-1/6">
         <div>
             <div class="mb-10 flex justify-between">
                 <h4 class="font-bold">VMS App</h4>
                 <div class="md:hidden">
-                    <button @click="sideNav = false"><x-heroicon-s-chevron-left  class="w-6 h-6"/></button>
+                    <button @click="sideNav = false"><x-heroicon-s-chevron-left class="w-6 h-6"/></button>
                 </div>
             </div>
             <div class="flex flex-col gap-1 mb-3">
@@ -39,16 +39,16 @@
                     <h4 class="text-slate-400">General</h4>
                 </div>
                 <ul>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 active flex items-center gap-2"><x-heroicon-o-squares-2x2 class="w-5 h-5"/>Dashboard</li>
                     </a>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-calendar-date-range class="w-5 h-5"/>Visit</li>
                     </a>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-information-circle class="w-5 h-5"/>Regulation</li>
                     </a>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-user class="w-5 h-5"/>Guest</li>
                     </a>
                 </ul>
@@ -59,16 +59,16 @@
                     <h4 class="text-slate-400">Setting</h4>
                 </div>
                 <ul>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-squares-2x2 class="w-5 h-5"/>User</li>
                     </a>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-calendar-date-range class="w-5 h-5"/>Visit</li>
                     </a>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-information-circle class="w-5 h-5"/>Regulation</li>
                     </a>
-                    <a href="#" class="">
+                    <a href="#">
                         <li class="p-2 flex items-center gap-2 sideNavHover"><x-heroicon-o-user class="w-5 h-5"/>Guest</li>
                     </a>
                 </ul>
@@ -77,7 +77,7 @@
 
         <div class="p-2 flex justify-center items-center rounded-md bg-blue-950 hover:bg-black hover:text-white">
             Logout
-            <x-heroicon-s-arrow-right-start-on-rectangle  class="w-5 h-5 ms-3"/>
+            <x-heroicon-s-arrow-right-start-on-rectangle class="w-5 h-5 ms-3"/>
         </div>
     </div>
     {{-- Sidenav End --}}
@@ -89,7 +89,7 @@
             <button @click="sideNav = !sideNav" class=""><x-heroicon-s-bars-3 class="w-6 h-6" /></button>
             <div class="flex gap-2 items-center">
                 <div class="border rounded-full p-1 flex">
-                    <x-heroicon-o-sun  class="w-6 h-6"/>
+                    <x-heroicon-o-sun class="w-6 h-6"/>
                 </div>
                 <img src="https://via.placeholder.com/150" alt="Avatar" class="w-10 h-10 rounded-full border-2 border-white"/>
                 <div>
@@ -116,12 +116,13 @@
                         <p class="text-xs">Here's what's happening with your system today</p>
                     </div>
                     <div class="p-4 bg-sky-50 rounded-3xl flex flex-col gap-3">
+                        {{-- statistic start --}}
                         <div class="p-4 bg-white rounded-3xl flex flex-col gap-3">
                             <div class="flex justify-between items-center">
                                 <h3 class="font-semibold text-blue-900">Today Visit</h3>
                                 <a href="#">
                                     <button class="pt-1 pb-1 ps-3 pe-3 bg-sky-200 rounded-full flex items-center text-sm text-blue-950">All visits 
-                                        <x-heroicon-s-chevron-right  class="w-4 h-4"/>
+                                        <x-heroicon-s-chevron-right class="w-4 h-4"/>
                                     </button>
                                 </a>
                             </div>
@@ -130,7 +131,6 @@
                                 <div class="flex flex-col">
                                     <div class="flex items-center">
                                         <p class="font-semibold text-blue-900 me-2 text-lg flex items-center">35 <x-heroicon-o-arrow-trending-up class="w-5 h-5 ms-2" /></p>
-                                        {{-- <x-heroicon-o-arrow-trending-up class="w-5 h-5" /> --}}
                                     </div>
                                     
                                     <div class="text-slate-400">
@@ -140,7 +140,6 @@
                                 <div class="flex flex-col">
                                     <div class="flex items-center">
                                         <p class="font-semibold text-blue-900 me-2 text-lg flex items-center">35 <x-heroicon-o-arrow-long-right class="w-5 h-5 ms-2" /></p>
-                                        {{-- <p class="font-semibold text-blue-900 me-2">30 </p><x-heroicon-o-arrow-long-right class="w-4 h-4"/> --}}
                                     </div>
                                     <div class="text-slate-400">
                                         <p class="text-xs">Yesterday Visit</p>
@@ -148,6 +147,40 @@
                                 </div>
                             </div>
                         </div>
+                        {{-- statistic end --}}
+
+                        {{-- statistic start --}}
+                        <div class="p-4 bg-white rounded-3xl flex flex-col gap-3">
+                            <div class="flex justify-between items-center">
+                                <h3 class="font-semibold text-blue-900">Today Visit</h3>
+                                <a href="#">
+                                    <button class="pt-1 pb-1 ps-3 pe-3 bg-sky-200 rounded-full flex items-center text-sm text-blue-950">All visits 
+                                        <x-heroicon-s-chevron-right class="w-4 h-4"/>
+                                    </button>
+                                </a>
+                            </div>
+                            <hr>
+                            <div class="flex items-center justify-between">
+                                <div class="flex flex-col">
+                                    <div class="flex items-center">
+                                        <p class="font-semibold text-blue-900 me-2 text-lg flex items-center">35 <x-heroicon-o-arrow-trending-up class="w-5 h-5 ms-2" /></p>
+                                    </div>
+                                    
+                                    <div class="text-slate-400">
+                                        <p class="text-xs">Visit</p>
+                                    </div>
+                                </div>
+                                <div class="flex flex-col">
+                                    <div class="flex items-center">
+                                        <p class="font-semibold text-blue-900 me-2 text-lg flex items-center">35 <x-heroicon-o-arrow-long-right class="w-5 h-5 ms-2" /></p>
+                                    </div>
+                                    <div class="text-slate-400">
+                                        <p class="text-xs">Yesterday Visit</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- statistic end --}}
                     </div>
                 </div>
                 {{-- End First Content --}}
@@ -156,7 +189,7 @@
                 <div class="flex flex-col gap-3">
                     <div class="flex justify-between items-center">
                         <h3 class="font-medium">Summary</h3>
-                        <button class="ps-3 pt-2 pe-3 pb-2 text-white flex items-center bg-blue-900 rounded-full"><h3 class="text-xs">Export </h3> <x-heroicon-o-arrow-down-on-square  class="w-4 h-4"/></button>
+                        <button class="ps-3 pt-2 pe-3 pb-2 text-white flex items-center bg-blue-900 rounded-full"><h3 class="text-xs">All Export</h3> <x-heroicon-o-arrow-down-on-square class="w-4 h-4"/></button>
                     </div>
                     <div class="flex flex-col gap-4 justify-between">
                         @livewire('chart-visit-component')
